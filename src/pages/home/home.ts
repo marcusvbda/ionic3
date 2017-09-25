f9fb48592b0c8f13cf56508f37174830c5bd579b
Marcus Vinicius Bassalobre de Assis <marcusv.bda@icloud.com>
2017-09-25 10:16:56 -0300

    app running

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,37 +1,44 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-@Component({
+@Component(
+{
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-items = [
-    'Pokémon Yellow',
-    'Super Metroid',
-    'Mega Man X',
-    'The Legend of Zelda',
-    'Pac-Man',
-    'Super Mario World',
-    'Street Fighter II',
-    'Half Life',
-    'Final Fantasy VII',
-    'Star Fox',
-    'Tetris',
-    'Donkey Kong III',
-    'GoldenEye 007',
-    'Doom',
-    'Fallout',
-    'GTA',
-    'Halo'
+export class HomePage
+{
+  items:any = 
+  [
+    {date:"8/4/1992",text:"NOTE EXEMPLE"}
   ];
 
-  constructor(public navCtrl: NavController) {
+  noteText:string;
+
+  constructor(public navCtrl: NavController) 
+  {
 
   }
 
-  itemSelected(item: string) {
-    console.log("Selected Item", item);
+  public addNote()
+  {
+    console.log(this.items[0]);
+    if(this.noteText)
+    {
+      var d = new Date();
+      this.items.push(
+      {
+        date: d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear(),
+        text:this.noteText
+      });
+      this.noteText="";  
+      console.log(this.items);     
+    }    
+  }
+
+  public deleteNote(key)
+  {
+    this.items.splice(key,1);    
   }
 
 }
